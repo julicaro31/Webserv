254912fd0afcb1fae03a9f9edc99562c408d48f9
Julia Caro <jcaro@f1r1s6.codam.nl>
2025-02-15 17:10:23 +0100

    initial parsing to create servers, it is not finished yet

--- include/ParsingHelper.hpp
+++ include/ParsingHelper.hpp
@@ -6,6 +6,7 @@ class ConfigBlock;
 #include "Directives.hpp"
 #include "Request.hpp"
 #include "Method.hpp"
+#include "Server.hpp"
 
 #include <fstream>
 #include <sstream>
@@ -22,6 +23,8 @@ public:
 	static std::string toString(Context context);
 	static std::vector<std::string> split(const std::string &str, char delimiter);
 
+	static std::vector<Server> createServers(ConfigBlock &configBlock);
+
 	static Request parseRequest(std::string strRequest);
 
 private:
@@ -31,6 +34,9 @@ private:
 	static ConfigBlock parseBlock(std::ifstream &file, std::string blockName, int braceLevel = 0);
 	static void handleSubBlock(std::ifstream &file, ConfigBlock &block, std::string blockName, std::string line);
 	static void handleDirective(ConfigBlock &block, std::string blockName, std::string line);
+
+	// Helper functions to create the servers from the configuration blocks.
+	static std::pair<std::string, int> parseHostAndPort(std::vector<std::string> &info);
 
 	static Method parseMethod(std::string method);
 };
