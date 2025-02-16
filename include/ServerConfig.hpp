ab1d874d42a8bf7d75f32ec8a0061dec31a4624f
Naji Kanounji <nakanoun@student.codam.nl>
2025-02-16 14:14:18 +0100

    defaultServer to struct

--- include/ServerConfig.hpp
+++ include/ServerConfig.hpp
@@ -4,11 +4,12 @@
 #include <map>
 
 struct ServerConfig {
-    std::string host;
-    int port;
-    std::string root;
-    std::string index;
     bool autoIndex;
+    bool defaultServer;
     size_t maxBodySize;
+    int port;
+    std::string host;
+    std::string index;
+    std::string root;
     std::map<int, std::string> errorPages;
-};
+};;
