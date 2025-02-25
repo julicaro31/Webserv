7478a6491f4962282c8f7f585288a1bf1d1b2a1f
sebastianpenning <whimizera@mailbox.org>
2025-02-25 16:48:28 +0100

    added psuedocode for future structure

--- src/http_parser/HttpParser.cpp
+++ src/http_parser/HttpParser.cpp
@@ -2,29 +2,49 @@
 
 void HttpParser::parseRequest(std::string strRequest)
 {
-	std::stringstream streamStrRequest(strRequest);
-	std::string method, target, version;
+	//	symbol table
+	//	analysis
+	//
+	//	lexical analyzer
+	//		token stream
+	//			divide into lexemes
+	//			map to symbol table
+	//		pass to syntax analyzer
+	//
+	//	syntax analyzer
+	//		create syntax-tree of token stream
+	//
+	//	semantic analyzer
+	//		use syntax tree and symbol table
+	//		type checking
+	//
+	//	synthesis / code generation
+	//		map into request
+	
 
-	streamStrRequest >> method >> target >> version;
-
-	std::map<std::string, std::string> headers;
-	std::string header;
-	std::getline(streamStrRequest, header);
-	while (getline(streamStrRequest, header))
-	{
-		if (header == "\r" || header.empty())
-		{
-			break;
-		}
-
-		size_t colonPos = header.find(':');
-		if (colonPos != std::string::npos)
-		{
-			std::string headerName = header.substr(0, colonPos);
-			std::string headerValue = std::string("lol");//ParsingHelper::trim(header.substr(colonPos + 1));
-			headers[headerName] = headerValue;
-		}
-	}
+	// std::stringstream streamStrRequest(strRequest);
+	// std::string method, target, version;
+	//
+	// streamStrRequest >> method >> target >> version;
+	//
+	// std::map<std::string, std::string> headers;
+	// std::string header;
+	// std::getline(streamStrRequest, header);
+	// while (getline(streamStrRequest, header))
+	// {
+	// 	if (header == "\r" || header.empty())
+	// 	{
+	// 		break;
+	// 	}
+	//
+	// 	size_t colonPos = header.find(':');
+	// 	if (colonPos != std::string::npos)
+	// 	{
+	// 		std::string headerName = header.substr(0, colonPos);
+	// 		std::string headerValue = std::string("lol");//ParsingHelper::trim(header.substr(colonPos + 1));
+	// 		headers[headerName] = headerValue;
+	// 	}
+	// }
 
 	// Request request(method, target, version, headers);
 
