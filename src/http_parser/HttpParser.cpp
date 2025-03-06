ada3f7ef156eb0f8b40c8d47f94d72be8c24cb28
spenning <whimizera@mailbox.org>
2025-03-06 18:13:02 +0100

    added more notes + start of pseudocode

--- src/http_parser/HttpParser.cpp
+++ src/http_parser/HttpParser.cpp
@@ -1,7 +1,5 @@
 #include "HttpParser.hpp"
 
-void HttpParser::parseRequest(std::string strRequest)
-{
 	//	symbol table
 	//	analysis
 	//
@@ -51,6 +49,15 @@ void HttpParser::parseRequest(std::string strRequest)
 	//				  / | \
 	//				X	Y  Z
 	//				The order from left to right matter for the nodes in the tree. 
+	//				From left to right, the leaves of a parse tree form the yield of the tree, which is the string generated / 
+	//				derived from the nonterminal.
+	//				  list
+	//				 /
+	//				list
+	//				|
+	//				digit
+	//				|
+	//				9
 	//		token stream
 	//			divide into lexemes
 	//				lexem = {tokenName, attributeValue}
@@ -68,34 +75,12 @@ void HttpParser::parseRequest(std::string strRequest)
 	//
 	//	synthesis / code generation
 	//		map into request
-	
 
-	// std::stringstream streamStrRequest(strRequest);
-	// std::string method, target, version;
-	//
-	// streamStrRequest >> method >> target >> version;
-	//
-	// std::map<std::string, std::string> headers;
-	// std::string header;
-	// std::getline(streamStrRequest, header);
-	// while (getline(streamStrRequest, header))
-	// {
-	// 	if (header == "\r" || header.empty())
-	// 	{
-	// 		break;
-	// 	}
-	//
-	// 	size_t colonPos = header.find(':');
-	// 	if (colonPos != std::string::npos)
-	// 	{
-	// 		std::string headerName = header.substr(0, colonPos);
-	// 		std::string headerValue = std::string("lol");//ParsingHelper::trim(header.substr(colonPos + 1));
-	// 		headers[headerName] = headerValue;
-	// 	}
-	// }
+void HttpParser::parseRequest(std::string strRequest)
+{
+	wordStream words;
 
-	// Request request(method, target, version, headers);
-
-	// return Request();
+	words = Scanner(strRequest);
+	parse(words);
 }
 
