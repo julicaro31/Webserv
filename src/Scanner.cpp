0cf145c5451e5c7110ec4d8081b6df52b8c389ac
spenning <whimizera@mailbox.org>
2025-03-10 20:23:55 +0100

    set up files for scanner and token

---
+++ src/Scanner.cpp
@@ -0,0 +1,16 @@
+#include "Scanner.hpp"
+#include <vector>
+#include "Token.hpp"
+
+std::vector<Token> Scanner::scanTokens(void)
+{
+
+}
+
+Scanner::Scanner(void)
+{
+}
+
+Scanner::Scanner(const Scanner &scanner)
+{
+}
