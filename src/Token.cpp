b9557535440ce28260dfdfbb0ca95577e3b7b80b
spenning <whimizera@mailbox.org>
2025-03-11 17:54:07 +0100

    expanded constructor + added toString

--- src/Token.cpp
+++ src/Token.cpp
@@ -1,7 +1,16 @@
 #include "Token.hpp"
 
-Token::Token()
+std::string toString(void)
 {
+	return (type + " " + lexeme + " " + literal);
+}
+
+Token::Token(TokenType type, std::string lexeme, std::string literal, int line)
+{
+	this->type = type;
+	this->lexeme = lexeme;
+	this->literal = literal;
+	this->line = line;
 }
 
 Token::Token(const Token &token)
