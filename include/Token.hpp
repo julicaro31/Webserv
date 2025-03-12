6a9fa703685c01249219ff65d004f5ad77436167
spenning <whimizera@mailbox.org>
2025-03-12 16:22:16 +0100

    added indentifier in Scanner + merged Token and TokenType + added useful tokens

--- include/Token.hpp
+++ include/Token.hpp
@@ -3,15 +3,34 @@
 
 #include <cctype>
 #include <string>
-#include "TokenType.hpp"
+#include <unordered_map>
 
 class Token
 {
+public:
+	enum TokenType
+	{
+		// single-char tokens
+		DOT, SLASH, QUESTION_MARK, EQUAL,
+		SINGLE_SPACE, COLON, STAR, CR, LF,
+
+		// One or two character tokens.
+		DOUBLE_SLASH, CRLF,
+
+		// Literals.
+		IDENTIFIER, STRING, NUMBER, METHOD,
+		REQUEST_TARGET, HTTP_VERSION, WHITESPACE,
+
+		// Keywords.
+		WHERE,
+		EOF,
+	};
 private:
 	TokenType	type;
 	std::string lexeme;
 	std::string	literal;
 	int			line;
+	static const std::unordered_map<std::string, TokenType> keywords;
 public:
 	std::string toString();
 	Token(TokenType, std::string, std::string, int);
