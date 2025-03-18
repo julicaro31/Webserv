a1c4ee5f1f23772de9e5cc6a674fcc218186e717
sebastianpenning <whimizera@mailbox.org>
2025-03-18 11:45:58 +0100

    added clangd files for nvim

---
+++ cmake/CMakeFiles/my_app.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_app.dir/src/Binary.cpp.o"
+  "CMakeFiles/my_app.dir/src/Binary.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/HttpParser.cpp.o"
+  "CMakeFiles/my_app.dir/src/HttpParser.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/Scanner.cpp.o"
+  "CMakeFiles/my_app.dir/src/Scanner.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/Server.cpp.o"
+  "CMakeFiles/my_app.dir/src/Server.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/ServerManager.cpp.o"
+  "CMakeFiles/my_app.dir/src/ServerManager.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/Token.cpp.o"
+  "CMakeFiles/my_app.dir/src/Token.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/configuration_file/ConfigBlock.cpp.o"
+  "CMakeFiles/my_app.dir/src/configuration_file/ConfigBlock.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/configuration_file/Directives.cpp.o"
+  "CMakeFiles/my_app.dir/src/configuration_file/Directives.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/main.cpp.o"
+  "CMakeFiles/my_app.dir/src/main.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/parsing_helper/ParsingHelper.cpp.o"
+  "CMakeFiles/my_app.dir/src/parsing_helper/ParsingHelper.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/request/Request.cpp.o"
+  "CMakeFiles/my_app.dir/src/request/Request.cpp.o.d"
+  "CMakeFiles/my_app.dir/src/test_server.cpp.o"
+  "CMakeFiles/my_app.dir/src/test_server.cpp.o.d"
+  "my_app"
+  "my_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
