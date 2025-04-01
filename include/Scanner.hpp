6a3b5b7052590880808796e120402633f6f9b0cb
sebastianpenning <whimizera@mailbox.org>
2025-04-01 18:33:13 +0200

    reformat scanner + added [[fallthrough]] + destructor

--- include/Scanner.hpp
+++ include/Scanner.hpp
@@ -4,18 +4,22 @@
 #include "Token.hpp"
 #include <cctype>
 #include <vector>
-		
 
 #define SPACE 13
 #define	CR 32
 
 class Scanner
 {
+public:
+	Scanner(std::string);
+	Scanner(const Scanner &);
+	~Scanner(void);
+	std::vector<Token> scanTokens(void);
 private:
 	int	start = 0;
 	int	current = 0;
 	int	line = 1;
-	std::unordered_map<std::string, Token::TokenType> keywords = {
+	const std::unordered_map<std::string, Token::TokenType> keywords = {
 	{"GET", Token::TokenType::GET},
 	{"POST", Token::TokenType::POST},
 	{"DELETE", Token::TokenType::DELETE},
@@ -28,13 +32,12 @@ private:
 	};
 	std::string source;
 	std::vector<Token> tokens;
-	bool isAtEnd(void);
-	bool isAtEnd(int);
+private:
 	void scanToken();
-	char advance();
-	bool match(char);
-	void string();
+	void addToken(Token::TokenType);
+	void addToken(Token::TokenType, std::string);
 	void version();
+	void string();
 	void number();
 	void uri();
 	bool header();
@@ -42,12 +45,9 @@ private:
 	char peek();
 	char peek(int);
 	char peekNext();
-	void addToken(Token::TokenType);
-	void addToken(Token::TokenType, std::string);
-public:
-	std::vector<Token> scanTokens(void);
-	Scanner(std::string);
-	Scanner(const Scanner &);
+	bool isAtEnd(void);
+	bool isAtEnd(int);
+	char advance();
 };
 
 #endif
