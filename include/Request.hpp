81c6551cce3e97d5045684067b8fb1ff7888a634
sebastianpenning <whimizera@mailbox.org>
2025-04-01 19:38:31 +0200

    draft of request class

---
+++ include/Request.hpp
@@ -0,0 +1,34 @@
+#ifndef __REQUEST_H__
+#define __REQUEST_H__
+
+#include "Token.hpp"
+#include <vector>
+
+class Request
+{
+public:
+	enum class Method
+	{
+		NONE,
+		GET,
+		POST,
+		DELETE,
+	};
+	Method getMethod(void) const;
+	float getVersion(void) const;
+	std::string getUri(void) const;
+	std::vector<std::pair<std::string, std::string>> getHeaders(void) const;
+	std::string getBody(void) const;
+	Request(const Request &);
+	Request(std::vector<Token>);
+	~Request(void);
+private:
+	Method method;
+	float version;
+	std::string uri;
+	std::vector<std::pair<std::string, std::string>> headers;
+	std::string body;
+};
+
+
+#endif
