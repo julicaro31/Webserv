ace4b69058cd58aca1cd77938a1ad8e962b0eb19
sebastianpenning <whimizera@mailbox.org>
2025-04-02 17:45:18 +0200

    add cases for request

--- src/Request.cpp
+++ src/Request.cpp
@@ -1,5 +1,6 @@
 #include "Request.hpp"
 #include "Token.hpp"
+#include <utility>
 
 Request::Request(std::vector<Token> tokens)
 {
@@ -7,17 +8,39 @@ Request::Request(std::vector<Token> tokens)
 	{
 		Token::TokenType type = it.getType();
 		switch(type){
-			case Token::METHOD: break;
-			case Token::VERSION: break;
+			case Token::METHOD:
+				{
+					std::string lexeme = it.getLexeme();
+					if (lexeme == "GET")
+						method = Method::GET;
+					else if (lexeme == "POST")
+						method = Method::POST;
+					else if (lexeme == "DELETE")
+						method = Method::DELETE;
+					else
+						method = Method::NONE;
+				}
+				break;
+			case Token::VERSION: 
+				version = std::stof(it.getLiteral());
+				break;
 			case Token::URI: break;
+				uri = it.getLiteral();	
 			case Token::HEADER: break;
+				{
+					std::string header = it.getLiteral();
+					std::size_t delimiter = header.find(':');
+					std::string header_name = header.substr(0, delimiter);
+					std::string header_value = header.substr(delimiter, header.size() - 1);
+					headers.push_back(std::make_pair(header_name, header_value));
+				}
 			case Token::BODY: break;
 		default:
-
+			break;
 		}
 	}
 }
 
-~Request(void)
+Request::~Request(void)
 {
 }
