9b0c229f66f0c033dcbb5fc6ed740aeaea427e49
sebastianpenning <whimizera@mailbox.org>
2025-04-29 17:13:06 +0200

    setup handle request in serverManager

--- src/main.cpp
+++ src/main.cpp
@@ -15,92 +15,54 @@ int main(int ac, char *argv[])
 	Logger::init("logs.log");
 	if (ac < 2)
 	{
-		std::cout << "Wrong number of arguments." << std::endl;
+		std::cout << "Wrong number of arguments. Too few arguments" << std::endl;
 		return -1;
-	}
-	if (ac == 2)
-	{
-		Logger::log(INFO, "Parsing configuration file...");
-		std::string line = "DELETE /hello.txt HTTP/1.1\r\nHost: example.com\r\nUser-Agent: Mozilla/5.0\r\nAccept: text/html\r\n\r\n{ \"username\": \"user123\", \"password\": \"pass123\"}\r\n";
-		try
-		{
-			tokens = HttpParser::parseRequest(line);
-		}
-		catch (const char *exception)
-		{
-			std::cerr << "Error: " << exception << std::endl;
-		}
-		catch (...)
-		{
-			std::cerr << "Unknown error" << std::endl;
-		}
-		Request reqst = Request(tokens);
-		DEBUG_PRINT(Request::requestToString(reqst));
-		try
-		{
-			std::string filePath(argv[1]);
-			std::vector<ServerConfig> serversConfiguration = ParsingHelper::getServersConfig(filePath);
-			ServerManager serverManager;
-			for (std::vector<ServerConfig>::iterator it = serversConfiguration.begin(); it != serversConfiguration.end(); it++)
-			{
-				serverManager.addServer(*it);
-			}
-			serverManager.printServers();
-
-			if (serverManager.getServers().size() > 0)
-			{
-				// TEST RESPONSE WITH FIRST SERVER (example)
-				Request r(tokens);
-				testResponse(r, *serverManager.getServers()[0]);
-			}
-		}
-		catch (const std::exception &e)
-		{
-			std::cerr << e.what() << '\n';
-		}
 	}
 	else if (ac == 3)
 	{
-		std::ifstream file(argv[2]);
-		if (!file.is_open())
-		{
-			std::cerr << "Error opening file: " << argv[2] << std::endl;
-			return -1;
+		std::vector<ServerConfig> serversConfiguration;
+
+		std::cout << "<< DEV version >>" << std::endl;
+		Logger::log(INFO, "Starting server...");
+		Logger::log(INFO, "Setting up configuration");
+
+		std::string filePath(argv[2]);
+		try {
+			serversConfiguration = ParsingHelper::getServersConfig(filePath);
+		} catch (const std::exception &e) {
+			std::cerr << "failed to parse due to " << e.what() << std::endl;
 		}
-		std::stringstream buffer;
-		buffer << file.rdbuf();
-		std::string content = buffer.str();
-		HttpParser::parseRequest(content);
+		ServerManager serverManager;
+		for (std::vector<ServerConfig>::iterator it = serversConfiguration.begin(); it != serversConfiguration.end(); it++)
+		{
+			serverManager.addServer(*it);
+		}
+		serverManager.printServers();
+		
+		Logger::log(INFO, "finished configuration");
+		if (serverManager.getServers().size() > 0)
+		{
+			if ((argv[1]) == std::string("start"))
+			{
+				Logger::log(INFO, "serverManager setting up poll");
+				serverManager.runPoll();
+			}
+			else
+			{
+				std::cerr << "Wrong type of arguments." << std::endl;
+				return -1;
+			}
+		}
+		else
+		{
+			std::cerr << "configuration failed" << std::endl;
+			return (-1);
+		}
 	}
 	else
 	{
-		std::cout << "<< DEV version >>" << std::endl;
-		Logger::log(INFO, "Starting server...");
-		std::vector<Location> locations;
-		ServerConfig config = {
-			8080,
-			false,
-			true,
-			1000000,
-			"localhost",
-			"/html",
-			"name",
-			{302, "http.."},
-			{{400, "error400.html"},
-			 {403, "error403.html"},
-			 {501, "error50x.html"},
-			 {502, "error50x.html"}},
-			{"index.html"},
-			locations};
-
-		ServerManager serverManager;
-		serverManager.addServer(config);
-		serverManager.printServers();
-		if ((argv[1]) == std::string("start"))
-		{
-			serverManager.runPoll();
-		}
+		std::cerr << "Wrong number of arguments. Too many arguments" << std::endl;
+		return -1;
 	}
-
 	return 0;
 }
