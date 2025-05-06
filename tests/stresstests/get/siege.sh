56ad8b36af5344e9048f186d2c2a2a802700572e
spenning <whimizera@mailbox.org>
2025-05-06 15:17:05 +0200

    fix exit codeif statement + post tests added

--- tests/stresstests/get/siege.sh
+++ tests/stresstests/get/siege.sh
@@ -15,7 +15,7 @@ for conc in ${conditions[@]}; do
         echo -ne "concurrent $conc - reps $rep: "
         siege -m "concurrent $conc - reps $rep" --delay=0.5 --file=$urls --log=$log --internet --verbose --reps=$rep --concurrent=$conc --no-parser &>> $test_log
         exit_code=$(echo $?)
-        if [[ exit_code != 0 ]]; then
+        if [[ $exit_code != 0 ]]; then
             echo " ❌"
             test_exit_code=1
         else
--- tests/stresstests/post/siege.sh
+++ tests/stresstests/post/siege.sh
@@ -15,9 +15,10 @@ for conc in ${conditions[@]}; do
         echo -ne "concurrent $conc - reps $rep: "
         siege -m "concurrent $conc - reps $rep" --delay=0.5 --file=$urls --log=$log --internet --verbose --reps=$rep --concurrent=$conc --no-parser &>> $test_log
         exit_code=$(echo $?)
-        if [[ exit_code != 0 ]]; then
+        if [[ $exit_code != 0 ]]; then
             echo " ❌"
             test_exit_code=1
+            echo $exit_code
         else
             echo " ✅" 
         fi
