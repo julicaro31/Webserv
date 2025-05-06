091635f688f389a61dd2d2645f4e8e658616d3ad
spenning <whimizera@mailbox.org>
2025-05-06 16:10:51 +0200

    added cgi directory

---
+++ html/python_cgi/hello.py
@@ -0,0 +1,9 @@
+
+print ( "Content-type: text/html\n\n" )
+print ( "<html>\n" )
+print ( "<body>\n" )
+print ( "<h1>Hello there!</h1>\n" )
+print ( "</body>\n" )
+print ( "</html>\n" )
+
+exit ( 0 )
