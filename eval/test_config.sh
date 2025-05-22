86798d95e24b25035aa303cef8873cdf0c2debd4
sebastianpenning <whimizera@mailbox.org>
2025-05-22 11:01:41 +0200

    added test_configs

---
+++ eval/test_config.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+exe="./../webserv"
+
+incorrect_folder="configs/incorrect"
+correct_folder="configs/correct"
+incorrects=($incorrect_folder/*)
+corrects=($correct_folder/*)
+extra_arg="start"
+test_exit_code=0
+
+#prepare executable
+echo -e "preparing executable for testing\n"
+(cd ../ && make)
+
+echo -e "\nincorrect config file tests\n"
+counter=0
+for i in "${incorrects[@]}"
+do
+    echo -n "$counter: $(basename $i)"
+
+    args="${extra_arg} ${i}"
+    timeout 1 $exe $args &> /dev/null
+    exit_code=$(echo $?)
+    
+    if [ $exit_code == 124 ]; then
+        echo " ❌"
+        test_exit_code=1
+    else
+        echo " ✅" 
+    fi
+    counter=$((counter+1))
+done
+
+echo -e "\ncorrect config file tests\n"
+counter=0
+for i in "${corrects[@]}"
+do
+    echo -n "$counter: $(basename $i)"
+
+    args="${extra_arg} ${i}"
+    timeout 1 $exe $args &> /dev/null
+    exit_code=$(echo $?)
+    
+    if [ $exit_code != 124 ]; then
+        echo " ❌"
+        test_exit_code=1
+    else
+        echo " ✅" 
+    fi
+    counter=$((counter+1))
+done
+
+
+
+echo
+if [ $test_exit_code == 1 ]; then
+    echo "Not all tests passed!"
+else
+    echo "All tests passed!" 
+fi
+
+rm -rf logs.log
+
+exit $test_exit_code
