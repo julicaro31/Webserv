51284bd3498f0780c7140dd4d37aad2468390e93
sebastianpenning <whimizera@mailbox.org>
2025-06-09 17:01:26 +0200

    added curl -i to unknown method script

--- eval/unknown.sh
+++ eval/unknown.sh
@@ -2,4 +2,4 @@
 
 (cd ../ && gnome-terminal -- bash -c "./webserv ; bash")
 sleep 1
-curl -X UNKNOWN --data "but wait?" http://localhost:8080/test_unknown
+curl -i -X UNKNOWN --data "but wait?" http://localhost:8080/test_unknown
