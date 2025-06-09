142e567a7f4775d6ab8bab151042fc30601c5f3d
sebastianpenning <whimizera@mailbox.org>
2025-06-09 16:59:02 +0200

    added curl -i to timeout script

--- eval/timeout.sh
+++ eval/timeout.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+#timout test to see if we are not getting in a infinite loop checking an empty body even though Content-Length is set
+(cd ../ && gnome-terminal -- bash -c "./webserv ; bash")
+sleep 1
 curl -H "Content-Length: 2000" http://localhost:8080
